33987ff1bf0e8c8ebca7f9766b020e56882ff7df
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-02-06 09:05:29 -0500

    Usando hash router para el uso de rutas para evitar errores 404

--- src/components/ConceptosBasicos.jsx
+++ src/components/ConceptosBasicos.jsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  Link,
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  HashRouter,
+} from "react-router-dom";
 import Acerca from "../pages/Acerca";
 import Contacto from "../pages/Contacto";
 import Dashboard from "../pages/Dashboard";
@@ -20,6 +27,19 @@ const ConceptosBasicos = () => {
         quisquam eaque libero ea? Porro ratione amet minus nesciunt, nemo, nisi
         ducimus voluptatum vel eius numquam quibusdam laborum rem quia dolor.
       </p>
+
+      <HashRouter>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/acerca">Acerca</Link>
+          <Link to="/contacto">Contacto</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/acerca" element={<Acerca />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
+      </HashRouter>
 
       <BrowserRouter>
         <MenuConceptos />
